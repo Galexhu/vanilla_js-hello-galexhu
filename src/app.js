f919ead48587097cc98e2734f6675dab94f50f8d
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-08-09 12:32:26 +0000

    cambios ok

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandom(anyArray) {
+  let max = who.length;
+
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = who[0] + " " + action[2] + " " + what[1] + " " + when[3];
 };
