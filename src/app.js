1871925c9e543928d38bc64ab6d858df84a93f59
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-08-10 18:12:47 +0000

    final_fix

--- src/app.js
+++ src/app.js
@@ -5,18 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pajaro'];
+let action = ['se comio', 'hizo pis', 'aplasto', 'rompio'];
+let what = ['mis deberes', 'mi telefono', 'el coche'];
+let when = ['antes de clase', 'mientras dormia', 'mientras hacia ejercicio', 'durante mi merienda', 'mientras estaba rezando'];
+
+function getRandomNumber(a,b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
 
 function getRandom(anyArray) {
-  let max = who.length;
-
+  let max = anyArray.length;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
 }
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  document.querySelector("#excuse").innerHTML = who[0] + " " + action[2] + " " + what[1] + " " + when[3];
+  document.querySelector("#excuse").innerHTML = getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when);
 };
